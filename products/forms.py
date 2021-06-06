319d049613c43c562fbd509c9670e73cae0457f1
EllenSt264 <stafford.ellen@protonmail.com>
2021-06-06 08:22:01 +0000

    Update class name for admin product form

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Category, Product
 
 
-class ProductModel(forms.ModelForm):
+class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
