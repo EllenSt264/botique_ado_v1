a859a4699b602aa997fe4176e4f5948537540e16
EllenSt264 <stafford.ellen@protonmail.com>
2021-06-06 17:42:26 +0000

    Add edit product functionality for admin

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # from interpreting the string as the id, which would throw an error
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>', views.edit_product, name='edit_product'),
 ]
