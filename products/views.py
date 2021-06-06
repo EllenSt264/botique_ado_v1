2e6fd4375fbba23462fa83d38b25a1361e1b979d
EllenSt264 <stafford.ellen@protonmail.com>
2021-06-06 08:54:58 +0000

    Update add product functionality for admin

--- products/views.py
+++ products/views.py
@@ -91,6 +91,20 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Admin - Add product to store """
 
+    if request.method == "POST":
+        # Include request.FILES for image submisson
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid')
+    # Move this empty form instantiation to an else block so it doesn't
+    # wipe out the form errors
+    else:
+        form = ProductForm()
+
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
--- profiles/views.py
+++ profiles/views.py
@@ -16,9 +16,11 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    # Populate form with user's profile information
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Updated failed. Please ensure the form is valid')
+    else:
+        # Populate form with user's profile information
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
